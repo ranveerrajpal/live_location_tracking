edc96976b0254cd06946018bd76af22cfceef09e
ranveerrajpal <ranveerrajpal19@gmail.com>
2024-10-01 22:37:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 from flask_socketio import SocketIO
 import os
 
@@ -6,10 +6,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 socketio = SocketIO(app)
 
-# Route to serve the frontend
+# Route to serve the index.html directly
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory(os.getcwd(), 'index.html')
 
 # WebSocket event handler for receiving sensor data
 @socketio.on('sensor_data')
@@ -18,6 +18,5 @@ def handle_sensor_data(data):
     # Send back a response to the frontend
     socketio.emit('server_response', {'message': 'Data received successfully'})
 
-# Remove the run block
 if __name__ == '__main__':
     socketio.run(app, debug=True, host="0.0.0.0", port=int(os.getenv('PORT', 5000)))
